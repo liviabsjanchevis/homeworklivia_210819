e5d5b125e75c3612393a23bbd114175f0fa70164
liviabsjanchevis <88117484+liviabsjanchevis@users.noreply.github.com>
2021-08-18 21:58:24 -0300

    Add files via upload

---
+++ livia_210816.py
@@ -0,0 +1,35 @@
+print("Question2********")
+words=["computer","violin","cellphone","eraser","floor","television","water"]
+print(words)
+
+print("Question3***********")
+for livia in words[4:7]:
+    print(livia)
+
+print("Question4************")
+words.append("football")
+print(words)
+
+print("Question5*************")
+for livia in words:
+    if livia == "football":
+        print(livia.lower())
+    else:
+        print(livia.upper()+"!")
+
+print("Question6**************")
+for livia in words:
+    if livia != "football":
+        print(livia)
+
+print("QUestion7**************")
+list=[]
+for banana in range(10,21):
+    print(banana)
+
+print("Question8*****************")
+for banana in range(10,21):
+    if banana < 16:
+        print(banana)
+
+print("Question9***************")
